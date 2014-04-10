8496a6d1e141cef1bdbba4918e2beef53ae5b4c3
Nate Hunzaker <nate.hunzaker@gmail.com>
2014-04-09 20:53:36 -0400

    Tweak walker.js

--- walk/walker.js
+++ walk/walker.js
@@ -4,23 +4,27 @@ class Walker {
 	constructor() {
 		this.x = canvas.width / 2;
 		this.y = canvas.height / 2
+        this.speed = 4;
 	}
 
 	display() {
 		ctx.globalAlpha = 0.4;
-		ctx.fillRect(this.x, this.y, 1, 1);
+        ctx.beginPath();
+		ctx.arc(this.x, this.y, 1, 0, Math.PI * 2);
+        ctx.fill();
 	}
 
 	step() {
-		this.x += Math.random() * 4 - 2
-		this.y += Math.random() * 4 - 2
+		this.x += Math.random() * this.speed - (this.speed * 0.5);
+		this.y += Math.random() * this.speed - (this.speed * 0.5);
 	}
 };
 
-let w = new Walker();
+let walkers = [for (x of new Array(5)) new Walker()];
 
 requestAnimationFrame(function loop() {
-    w.step();
-	w.display();
+    ctx.save();
+    walkers.map((w) => { w.step(); w.display(); });
+    ctx.restore();
 	requestAnimationFrame(loop);
 });
